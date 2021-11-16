6d89bcfe6991805b335792d202b95b1e51548e1e
Orlando Urbano Trejo <91504341+OrlandoUrbanoTrejov@users.noreply.github.com>
2021-11-16 11:37:35 -0600

    Add files via upload

---
+++ Ejercicio_2.m
@@ -0,0 +1,20 @@
+% Octave Script
+% Title		       :Función Inyectiva, Sobreyectiva, Biyectiva (Ejercicio_2)
+% Correo         :urbanoorlando79@gmail.com
+% Description	   :Script para determinar que tipo de función es dependiendo el dominio de la función
+% Author	       :Orlando Urbano Trejo (Lando)
+% Date		       :16_11_2021
+% Version	       :1
+% Usage		       :octave>path/Ejercicio_2
+%		             :Requiere aplicación octave, usar linea de comandos
+%		             :https://octaveintro.readthedocs.io/en/latest/index.html
+
+clear
+% Dominio de la función
+x= -15:2:20
+% Regla de correspondencia
+fx=x.^3
+disp ('La función es inyectiva ya que ?x,y?D | f(x) = f(y) ? x = y ');
+% Plotear función
+plot (x, fx)
+
