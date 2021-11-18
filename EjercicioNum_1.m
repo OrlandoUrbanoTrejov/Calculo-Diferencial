1f99f253e5bf746ab760bc0f4feaccd8ecaf4883
Orlando Urbano Trejo <91504341+OrlandoUrbanoTrejov@users.noreply.github.com>
2021-11-18 13:30:29 -0600

    Add files via upload

---
+++ EjercicioNum_1.m
@@ -0,0 +1,20 @@
+% Octave Script
+% Title		       :Funciones Algebraicas: Polinomiales y Racionales (EjercicioNum_1)
+% Correo         :urbanoorlando79@gmail.com
+% Description	   :Script para graficar funicones y ver si son funciones polinomiales o racionales.
+% Author	       :Orlando Urbano Trejo (Lando)
+% Date		       :17_11_2021
+% Version	       :1
+% Usage		       :octave>path/EjercicioNum_1
+%		             :Requiere aplicación octave, usar linea de comandos
+%		             :https://octaveintro.readthedocs.io/en/latest/index.html
+
+clear
+% Dominio de la función
+x= -4:1:4
+% Regla de correspondencia
+fx=(x+2).* (x-2) 
+disp ('La expresión corresponde a una Función Polinomica');
+disp ('Los puntos donde f(x)=0 son los puntos (0,-4), (-2,0) y (0,2)');
+% Plotear función
+plot (x, fx)
