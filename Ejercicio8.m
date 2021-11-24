d0b50998d65cbde761530c90bfba020eb6bc35d0
Orlando Urbano Trejo <91504341+OrlandoUrbanoTrejov@users.noreply.github.com>
2021-11-24 11:32:19 -0600

    Update Ejercicio8.m

--- Ejercicio8.m
+++ Ejercicio8.m
@@ -1,23 +1,23 @@
 % Octave Script
-% Title		       :Funciones Trascendentes: Trigonométricas, Logarítmicas y Exponenciales (Ejercicio_7)
-% Correo         :urbanoorlando79@gmail.com
-% Description	   :Script para graficar funicones y determinar que tipo de funcion es.
-% Author	       :Orlando Urbano Trejo (Lando)
-% Date		       :23_11_2021
-% Version	       :1
-% Usage		       :octave>path/Ejercicio_7
-%		             :Requiere aplicación octave, usar linea de comandos
-%		             :https://octaveintro.readthedocs.io/en/latest/index.html
+% Title           :Funciones Trascendentes: TrigonomÃ©tricas, LogarÃ­tmicas y Exponenciales (Ejercicio8)
+% Correo          :urbanoorlando79@gmail.com
+% Description     :Script para graficar funicones y determinar que tipo de funcion es.
+% Author          :Orlando Urbano Trejo (Lando)
+% Date            :23_11_2021
+% Version         :1
+% Usage           :octave>path/Ejercicio8
+%                 :Requiere aplicaciÃ³n octave, usar linea de comandos
+%                 :https://octaveintro.readthedocs.io/en/latest/index.html
 
 clear
-disp ('Función7');
-% Dominio de la función
+disp ('FunciÃ³n8');
+% Dominio de la funciÃ³n
 x=-50:1:50
 % Regla de correspondencia
 fx = cos(x)
-disp ('La función coseno es una funcion real');
-disp ('El dominio de la función es (-?, ?)');
-disp ('El rango de la función es de [-1,1]');
-disp ('Los puntos donde f(x)=0 son los puntos (2?, 0), (0,0)');
-% Plotear función
+disp ('La funciÃ³n coseno es una funcion real');
+disp ('El dominio de la funciÃ³n es (-âˆž, âˆž)');
+disp ('El rango de la funciÃ³n es de [-1,1]');
+disp ('Los puntos donde f(x)=0 son los puntos (Ï€/2 + 2Ï€n,0), (3Ï€/2 + 2Ï€n,0) y (0,1)');
+% Plotear funciÃ³n
 plot (fx)
