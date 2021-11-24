3aa0b87f2354e91c419dfbc09c375e19344c9662
Orlando Urbano Trejo <91504341+OrlandoUrbanoTrejov@users.noreply.github.com>
2021-11-24 10:49:25 -0600

    Add files via upload

---
+++ Ejercicio9.m
@@ -0,0 +1,23 @@
+% Octave Script
+% Title		       :Funciones Trascendentes: Trigonométricas, Logarítmicas y Exponenciales (Ejercicio_8)
+% Correo         :urbanoorlando79@gmail.com
+% Description	   :Script para graficar funicones y determinar que tipo de funcion es.
+% Author	       :Orlando Urbano Trejo (Lando)
+% Date		       :23_11_2021
+% Version	       :1
+% Usage		       :octave>path/Ejercicio_8
+%		             :Requiere aplicación octave, usar linea de comandos
+%		             :https://octaveintro.readthedocs.io/en/latest/index.html
+
+clear
+disp ('Función8');
+% Dominio de la función
+x=-30:1:30
+% Regla de correspondencia
+fx = tan(x)
+disp ('La función es sobreyectiva ya que C=|, es decir ?y ?| ?x?D| f(x)=y  ');
+disp ('El dominio de la función es [?n,?/2 + ?n) ? (?/2 + ?n,? + ?n)');
+disp ('El rango de la función es de (-?, ?)');
+disp ('Los puntos donde f(x)=0 son los puntos (?n, 0), (0,0)');
+% Plotear función
+plot (fx)
