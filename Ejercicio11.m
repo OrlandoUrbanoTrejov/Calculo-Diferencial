2bc5bce7ed484d1b0c46b9bdf1c11cce92828a5d
Orlando Urbano Trejo <91504341+OrlandoUrbanoTrejov@users.noreply.github.com>
2021-11-24 11:54:04 -0600

    Update Ejercicio11.m

--- Ejercicio11.m
+++ Ejercicio11.m
@@ -1,23 +1,23 @@
 % Octave Script
-% Title		       :Funciones Trascendentes: Trigonom�tricas, Logar�tmicas y Exponenciales (Ejercicio_10)
-% Correo         :urbanoorlando79@gmail.com
-% Description	   :Script para graficar funicones y determinar que tipo de funcion es.
-% Author	       :Orlando Urbano Trejo (Lando)
-% Date		       :23_11_2021
-% Version	       :1
-% Usage		       :octave>path/Ejercicio_10
-%		             :Requiere aplicaci�n octave, usar linea de comandos
-%		             :https://octaveintro.readthedocs.io/en/latest/index.html
+% Title           :Funciones Trascendentes: Trigonométricas, Logarítmicas y Exponenciales (Ejercicio11)
+% Correo          :urbanoorlando79@gmail.com
+% Description     :Script para graficar funciones y determinar que tipo de funcion es.
+% Author          :Orlando Urbano Trejo (Lando)
+% Date            :23_11_2021
+% Version         :1
+% Usage           :octave>path/Ejercicio11
+%                 :Requiere aplicación octave, usar linea de comandos
+%                 :https://octaveintro.readthedocs.io/en/latest/index.html
 
 clear
-disp ('Funci�n10');
-% Dominio de la funci�n
+disp ('Función11');
+% Dominio de la función
 x=-20:2:20
 % Regla de correspondencia
 hx = sec(x)
-disp ('La funci�n cosecante es una funci�n trigonometrica ');
-disp ('El dominio de la funci�n es (2pin,pi/2 + 2pin) U (pi/2 + 2pin,3pi/2 + 2pin) U 3pi/2 + 2pin, 2pi + 2 pin');
-disp ('El rango de la funci�n es de (-infinito, -1] U [1, infinito');
+disp ('La función cosecante es una función trigonometrica');
+disp ('El dominio de la función es (2πn, π/2 + 2πn) ∪ (π/2 + 2πn, 3π/2 + 2πn) ∪ (3π/2 + 2πn, 2π + 2 πn)');
+disp ('El rango de la función es de (-∞, -1] ∪ [1, ∞');
 disp ('Los puntos donde f(x)=0 son los puntos (0,1)');
-% Plotear funci�n
+% Plotear función
 plot (hx)
