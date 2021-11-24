241d12fdd5fa0d9378a66d1a656387fbbd4c9d3f
Orlando Urbano Trejo <91504341+OrlandoUrbanoTrejov@users.noreply.github.com>
2021-11-24 11:41:31 -0600

    Update Ejercicio9.m

--- Ejercicio9.m
+++ Ejercicio9.m
@@ -1,23 +1,23 @@
 % Octave Script
-% Title		       :Funciones Trascendentes: Trigonom�tricas, Logar�tmicas y Exponenciales (Ejercicio_8)
-% Correo         :urbanoorlando79@gmail.com
-% Description	   :Script para graficar funicones y determinar que tipo de funcion es.
-% Author	       :Orlando Urbano Trejo (Lando)
-% Date		       :23_11_2021
-% Version	       :1
-% Usage		       :octave>path/Ejercicio_8
-%		             :Requiere aplicaci�n octave, usar linea de comandos
-%		             :https://octaveintro.readthedocs.io/en/latest/index.html
+% Title           :Funciones Trascendentes: Trigonométricas, Logarítmicas y Exponenciales (Ejercicio9)
+% Correo          :urbanoorlando79@gmail.com
+% Description     :Script para graficar funicones y determinar que tipo de funcion es.
+% Author          :Orlando Urbano Trejo (Lando)
+% Date            :23_11_2021
+% Version         :1
+% Usage           :octave>path/Ejercicio9
+%                 :Requiere aplicación octave, usar linea de comandos
+%                 :https://octaveintro.readthedocs.io/en/latest/index.html
 
 clear
-disp ('Funci�n8');
-% Dominio de la funci�n
+disp ('Función9');
+% Dominio de la función
 x=-30:1:30
 % Regla de correspondencia
 fx = tan(x)
-disp ('La funci�n es sobreyectiva ya que C=|, es decir ?y ?| ?x?D| f(x)=y  ');
-disp ('El dominio de la funci�n es [?n,?/2 + ?n) ? (?/2 + ?n,? + ?n)');
-disp ('El rango de la funci�n es de (-?, ?)');
-disp ('Los puntos donde f(x)=0 son los puntos (?n, 0), (0,0)');
-% Plotear funci�n
+disp ('La función es sobreyectiva ya que C=|, es decir ∀y ∈| ∃x∈D| f(x)=y');
+disp ('El dominio de la función es [πn,π/2 + πn) ∪ (π/2 + πn,π + πn)');
+disp ('El rango de la función es de (-∞, ∞)');
+disp ('Los puntos donde f(x)=0 son los puntos (πn, 0), (0,0)');
+% Plotear función
 plot (fx)
