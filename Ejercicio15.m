273b98b80ea64f8fd71e2b736817a70b3c5554ff
Orlando Urbano Trejo <91504341+OrlandoUrbanoTrejov@users.noreply.github.com>
2021-11-24 12:23:15 -0600

    Update Ejercicio15.m

--- Ejercicio15.m
+++ Ejercicio15.m
@@ -1,23 +1,23 @@
 % Octave Script
-% Title		       :Funciones Trascendentes: Trigonométricas, Logarítmicas y Exponenciales (Ejercicio_14)
-% Correo         :urbanoorlando79@gmail.com
-% Description	   :Script para graficar funicones y determinar que tipo de funcion es.
-% Author	       :Orlando Urbano Trejo (Lando)
-% Date		       :23_11_2021
-% Version	       :1
-% Usage		       :octave>path/Ejercicio_14
-%		             :Requiere aplicación octave, usar linea de comandos
-%		             :https://octaveintro.readthedocs.io/en/latest/index.html
+% Title           :Funciones Trascendentes: TrigonomÃ©tricas, LogarÃ­tmicas y Exponenciales (Ejercicio15)
+% Correo          :urbanoorlando79@gmail.com
+% Description     :Script para graficar funciones y determinar que tipo de funcion es.
+% Author          :Orlando Urbano Trejo (Lando)
+% Date            :23_11_2021
+% Version	        :1
+% Usage           :octave>path/Ejercicio15
+%                 :Requiere aplicaciÃ³n octave, usar linea de comandos
+%                 :https://octaveintro.readthedocs.io/en/latest/index.html
 
 clear
-disp ('Función_14');
-% Dominio de la función
+disp ('FunciÃ³n15');
+% Dominio de la funciÃ³n
 x= -10:1:10
 % Regla de correspondencia
 fx=(e).^(9.* x + 18)
-disp ('La función es inyectiva ya que ?x,y?D | f(x) = f(y) ? x = y ');
-disp ('El dominio de la función es (-infinito, infinito)');
-disp ('El rango de la función es de (0, infinito)');
+disp ('La funciÃ³n es inyectiva ya que âˆ€x,yâˆˆD | f(x) = f(y) âŸ¹ x = y');
+disp ('El dominio de la funciÃ³n es (-âˆž, âˆž)');
+disp ('El rango de la funciÃ³n es de (0, âˆž)');
 disp ('Los puntos donde f(x)=0 son los puntos (0,e.^18');
-% Plotear función
+% Plotear funciÃ³n
 plot (x, fx)
